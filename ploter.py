7e46cf2fe49885324614881806986500f92d9bc1
Daniel Cano Alvira <pestr034-002@grupotr.es>
2023-11-20 17:26:16 +0100

    Improved

--- ploter.py
+++ ploter.py
@@ -7,7 +7,8 @@ import plotly.graph_objects as go
 
 
 def plotFunction(functions:(list or function), xstart:int, xstop:int, xstep:float=1, xname:str='x',
-                 ystart:int=None, ystop:int=None, ystep:float=1, yname:str='y') -> None:
+                 ystart:int=None, ystop:int=None, ystep:float=1, yname:str='y', zname:str='z'
+                 ) -> (go.Figure, pd.DataFrame):
     
     ystart = xstart if ystart == None else ystart
     ystop = xstop if ystop == None else ystop
@@ -15,9 +16,11 @@ def plotFunction(functions:(list or function), xstart:int, xstop:int, xstep:floa
     # In case there is a step of type float
     xmult = int('1' + '0'*len(str(xstep).split('.')[-1])) if '.' in str(xstep) else 1
     ymult = int('1' + '0'*len(str(ystep).split('.')[-1])) if '.' in str(ystep) else 1
+    
     # Generate the x and y axis
-    xaxis = [i/xmult for i in range(int(xstart), int(xstop*xmult), int(xstep*xmult))]
-    yaxis = [i/ymult for i in range(int(ystart), int(ystop*ymult), int(ystep*ymult))]
+    xaxis = [i/xmult for i in range(int(xstart*xmult), int(xstop*xmult), int(xstep*xmult))]
+    yaxis = [i/ymult for i in range(int(ystart*ymult), int(ystop*ymult), int(ystep*ymult))]
+    
     if len(yaxis) < len(xaxis):
         yaxis = list(np.linspace(ystart, ystop, len(xaxis)))
     elif len(yaxis) > len(xaxis):
@@ -51,21 +54,35 @@ def plotFunction(functions:(list or function), xstart:int, xstop:int, xstep:floa
             data[f'z{i}'] = temp.groupby('y')['z'].apply(list)
             data.reset_index(drop=False, inplace=True)
             series.append(go.Surface(z=data[f'z{i}'].values, x=data['x'], y=data['y']))
-
+            
         # If it has more than 3 dimensions raise an error
         else:
             raise ValueError('Function with more than 3 dimensions can\'t be ploted.')
 
+        
     # Generate chart to plot
     fig: go.Figure = go.Figure(data=series)
+
+    if dimensions == 1:
+        fig.update_xaxes(title=xname)
+        fig.update_yaxes(title=yname)
+
+    # If it is a 3D function
+    elif dimensions == 2:
+        fig.update_layout(
+            scene = {
+                'xaxis_title': xname,
+                'yaxis_title': yname,
+                'zaxis_title': zname
+            }
+        )
+
     fig.update_layout(title='Function plot', autosize=False,
-                  width=1000, height=700,
-                  margin=dict(l=50, r=50, b=20, t=50))
-    fig.update_xaxes(title=xname)
-    fig.update_yaxes(title=yname)
+                  width=700, height=700,
+                  margin=dict(l=20, r=20, b=20, t=30))
     fig.show()
 
-    return fig
+    return fig, data
 
 
 if __name__ == '__main__':
@@ -77,6 +94,9 @@ if __name__ == '__main__':
     # func3 = lambda x: (x.apply(lambda v: math.sin(2*v)) if isinstance(x, pd.Series) else math.sin(2*x))
     # func4 = lambda x: (x.apply(lambda v: math.sin(v) + 2) if isinstance(x, pd.Series) else math.sin(x) + 2)
 
-    func1 = lambda x, y: x * y - (1 - x) 
-    fig = plotFunction(func1, xstart=0, xstop=100, xstep=5, xname='Probabilida de Ganar', 
-                       ystart=0.5, ystop=6, ystep=0.2, yname='Ratio Beneficio-Riesgo')
+    func1 = lambda x, y: (x * y - (1 - x)) * 100
+    func2 = lambda x, y: (x - (1 - x)/y) * 100 if (x - (1 - x)/y) > 0 else 0
+    func2 = lambda x, y: np.where((x - (1 - x)/y) > 0, (x - (1 - x)/y) * 100, 0)
+    fig, data = plotFunction(func2, xstart=0.2, xstop=1, xstep=0.05, xname='Probabilidad de Ganar', 
+                       ystart=0.5, ystop=6, ystep=0.2, yname='Ratio Beneficio-Riesgo', 
+                       zname='Tamaño de Kelly')
